c7685e52c75cd8daaacf5c566655ec3c7826df03
thefriction <adityaaswal98@gmail.com>
2021-12-18 02:26:55 +0530

    NavBar Added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 
 function App() {
   return (
     <>
+    <Header/>
+    <main>
+      <Container>
       <h1>Welcome to Api DUkaan</h1>
+      </Container>
+    </main>    
+    <Footer/>
     </>
   );
 }
